6b14198572d7e2e0ae9991e51760613537193d8e
Muhammad Mahfud <laililmahvut@gmail.com>
2023-12-28 22:35:58 +0700

    fix

---
+++ resources/views/components/input/time-picker/asset.blade.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="{{asset('admin/css/flatpickr.css')}}">
+<script src="{{asset('admin/js/flatpickr.js')}}"></script>
+<script src="{{asset('admin/js/flatpick-init.js')}}"></script>
