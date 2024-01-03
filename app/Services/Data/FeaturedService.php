d9f9447a413cc0a1c317dde09cbd3de1a17cee2e
Naufal <nahanifg@gmail.com>
2024-01-03 15:11:40 +0700

    update

--- app/Services/Data/FeaturedService.php
+++ app/Services/Data/FeaturedService.php
@@ -18,7 +18,7 @@ class FeaturedService {
     public function getProductList($type, $limit = 0)
     {
         $data = $this->product::join("featured_products", "featured_products.product_id", "=", "products.id")
-        ->where("featured_products.category", $type)->whereDate("featured_products.start_at", ">=", Carbon::now())->whereDate("featured_products.expired_at", ">=", Carbon::now());
+        ->where("featured_products.category", $type)->whereDate("featured_products.start_at", "<=", Carbon::now())->whereDate("featured_products.expired_at", ">=", Carbon::now());
 
         if ($limit == 0) {
             return $data->get();
@@ -31,7 +31,7 @@ class FeaturedService {
     {
         $data = $this->merchant::join("featured_merchants", "featured_merchants.merchant_id", "=", "merchants.id")
         ->where("merchants.status", "active")
-        ->where("featured_merchants.category", $type)->whereDate("featured_merchants.start_at", ">=", Carbon::now())->whereDate("featured_merchants.expired_at", ">=", Carbon::now());
+        ->where("featured_merchants.category", $type)->whereDate("featured_merchants.start_at", "<=", Carbon::now())->whereDate("featured_merchants.expired_at", ">=", Carbon::now());
 
         if ($limit == 0) {
             return $data->get();
