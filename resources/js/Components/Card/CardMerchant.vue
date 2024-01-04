cf978943fc9eed17c3220b577b7d69463eec92a2
Naufal <nahanifg@gmail.com>
2024-01-04 21:16:23 +0700

    update

--- resources/js/Components/Card/CardMerchant.vue
+++ resources/js/Components/Card/CardMerchant.vue
@@ -13,8 +13,13 @@
         <p class="mb-0 text-xxs text-neutral-gray-2 line-clamp-2 whitespace-pre-wrap">{{ data.full_address }}</p>
         <p class="mb-1 text-xs text-orange whitespace-pre-wrap font-semibold">Komisi: {{ data.full_commission }}</p>
         <SolidBlue
-            @click="redirect"
-            class="!font-semibold !text-xs md:!text-sm !px-4 !py-2 justify-center cursor-pointer"
+            :href="route('guide.merchants.detail', {
+                id: data.uuid,
+                _query: {
+                    back_url
+                }
+            })"
+            class="!font-semibold !text-xs md:!text-sm !px-4 !py-2 justify-center"
         >
             Lihat Merchant
         </SolidBlue>
@@ -22,25 +27,14 @@
 </template>
 
 <script setup lang="ts">
-    import { router } from "@inertiajs/vue3";
     import SolidBlue from "../Button/SolidBlue.vue";
     import MerchantFill from "../Icon/Etc/MerchantFill.vue";
 
-    const props = defineProps<{
+    defineProps<{
         data: any;
         class?: any;
         back_url?: any;
+        id?: any;
     }>()
-
-    const redirect = () => {
-        router.visit(
-            route('guide.merchants.detail', {
-                id: props.data.id,
-                _query: {
-                    back_url: props.back_url
-                }
-            })
-        )
-    }
 
 </script>
