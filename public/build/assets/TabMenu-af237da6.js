e95259fe83554a15793a882fd2cb5dd6dc55806c
Naufal <nahanifg@gmail.com>
2024-01-06 22:26:08 +0700

    build

--- public/build/assets/TabMenu-af237da6.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./TabMenu.vue_vue_type_script_setup_true_lang-47627bb1.js";import"./app-8d83aa9a.js";export{m as default};
---
+++ public/build/assets/TabMenu-caa5d0df.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./TabMenu.vue_vue_type_script_setup_true_lang-f7026bf5.js";import"./app-c0e37fe6.js";export{m as default};
